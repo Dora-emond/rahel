0a31deea31a743c04cc8a6a480fdca1aa9bb5dd2
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2023-04-15 10:58:32 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,7 +21,12 @@ global.APIKeys = {
    'https://api.zeeoneofc.my.id': 'V12', // 👉 login https://api.zeeoneofc.my.id to get apikey
 }
 global.sewabotnya = `
-edit sewabot di file settings.js line 24
+Saya terus merenungi betapa rapuhnya kehidupan saya, 
+sebagai manusia tidak pernah terbayangkan sebelumnya 
+kehidupan saya yang begitu terhormat dalam sekejap 
+terperosok dalam nestapa dan kesulitan yang tidak 
+terperikan."Demikian lah penyesalan kerap tiba belakangan 
+tertinggal oleh amarah dan murka yang mendahului
 `
 // setting 
 global.firtsChat = true //send firts message to new user
@@ -46,17 +51,17 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Dira" //namabot kalian
+global.ownername = "ᴹᴿ᭄ Diraaa ×፝֟͜×" //nama kalian
+global.myweb = "" //bebas asal jan hapus
+global.youtube = "" //bebas asal jan hapus
+global.myweb2 = "" //bebas
+global.email = "prayogahendra60@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
+global.premium = ['6282387704390'] //premium user
+global.owner = ["6282387704390", "6289601145546"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6282387704390" // nomor wa kalian
 global.ownernomerr = "+"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
